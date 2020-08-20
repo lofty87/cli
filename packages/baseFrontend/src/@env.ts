9bbd390c8395046e410fd36045c57df90afb1f4c
lofty87 <lofty87@daum.net>
2020-08-20 19:54:37 +0900

    fix: baseFrontend @env
    1. 향 후 backend 와 합쳐질 것을 고려하여 환경변수 재정의

--- packages/baseFrontend/src/@env.ts
+++ packages/baseFrontend/src/@env.ts
@@ -2,5 +2,6 @@ export default {
   nodeEnv: process.env.REACT_APP_NODE_ENV || 'development',
   isDev: process.env.REACT_APP_NODE_ENV !== 'production',
   apiUrl: process.env.REACT_APP_API_URL as string,
-  imagesUrl: process.env.REACT_APP_IMAGES_URL as string,
+  assetsUrl: process.env.REACT_APP_ASSETS_URL as string,
+  imagesUrl: `${process.env.REACT_APP_ASSETS_URL}/images`,
 };
