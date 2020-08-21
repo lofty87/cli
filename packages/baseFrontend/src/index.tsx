99bc6f4f2db91d9c175aa73ac5ac7fe21145a265
lofty87 <lofty87@daum.net>
2020-08-21 12:27:56 +0900

    add, fix, remove: add modules, fix public dir
    1. material-ui (core, system)
    2. styled-components
    3. react-router-dom
    4. react-icons
    5. mobx, mobx-react
    6. axios
    7. 필요없는 파일들 삭제

--- packages/baseFrontend/src/index.tsx
+++ packages/baseFrontend/src/index.tsx
@@ -1,12 +1,2 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ThemeProvider } from '@material-ui/core';
