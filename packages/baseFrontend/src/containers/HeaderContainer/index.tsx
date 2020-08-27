3d1fd1239b33c0c77166080ae8d8f839ed94fbde
lofty87 <lofty87@daum.net>
2020-08-27 17:31:24 +0900

    fix: baseFrontend: props Issue...

--- packages/baseFrontend/src/containers/HeaderContainer/index.tsx
+++ packages/baseFrontend/src/containers/HeaderContainer/index.tsx
@@ -1,30 +1,17 @@
-import React, { useMemo, useState } from 'react';
+import React from 'react';
 import { Header } from '@components/organisms';
 
 import { Props } from './HeaderContainer.props';
 
 import { NCFC } from '$types/index';
 
-let count = 0;
-
 // * process async, state data
 const HeaderContainer: NCFC<Props> = ({
   match
-}) => {
-  const url = useMemo(() => match.url, [ match.url ]);
-  const [ countUrl, setCountUrl ] = useState(`${url} ${count}`);
-
-  setTimeout(() => {
-    count++;
-
-    setCountUrl(`${url} ${count}`);
-  }, 1000);
-
-  return (
-    <Header
-      $title={countUrl}
-    />
-  );
-};
+}) => (
+  <Header
+    $title={match.url}
+  />
+);
 
 export default HeaderContainer;
