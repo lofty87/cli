420da20a7a1c6b8deb39eb4834c521179286675f
lofty87 <lofty87@daum.net>
2020-09-08 13:13:34 +0900

    fix: copy package
    1. 개발환경과 실제 설치되었을 때 환경이 서로 달라 app-root-path 값이 최상위 node_modules 를 기준으로 하는 특성상 두 가지 방법으로 copy 를 처리하였다.

--- src/process/createProject.ts
+++ src/process/createProject.ts
@@ -4,16 +4,37 @@ import fs from 'fs-extra';
 import paths from '@config/paths';
 import { getPackageName } from '@lib/index';
 
+import { name as moduleName } from '../../package.json';
+
 import { PackageJson, ProjectType } from '$types/index';
 
+const { buildDir, packagesDir, rootDir } = paths;
+
+/**
+ * @name createProject
+ * * development 와 production 환경의 경로 구조가 달라서
+ * * package 를 copy 할 때 서로 다르게 처리했다.
+ *
+ * * build dir (use app-root-path)
+ * * development: '~/module/build'
+ * * production : '~/.npm/_npx/{random}/lib/node_modules/module/build'
+ */
 export const createProject = async (projectDir: string, projectType: ProjectType, packageJson: PackageJson) => {
-  const packagesDir = `${paths.buildDir}/${basename(paths.packagesDir)}`;
+  let packageDir;
+
   const packageName = getPackageName(projectType);
-  const packageDir = `${packagesDir}/${packageName}`;
 
-  await fs.copy(packageDir, projectDir);
+  try {
+    // ? development
+    packageDir = `${buildDir}/${basename(packagesDir)}/${packageName}`;
 
-  console.log(projectDir);
-  console.log(packageDir);
+    await fs.copy(packageDir, projectDir);
+  } catch(error) {
+    // ? production
+    packageDir = `${rootDir}/node_modules/${moduleName}/${basename(buildDir)}/${basename(packagesDir)}}/${packageName}`;
+
+    await fs.copy(packageDir, projectDir);
+  }
+
   console.log(packageJson);
 };
